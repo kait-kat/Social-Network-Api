cfc2175dcee4ad13fb23b1dc694fd91d1bc45cf9
kait <kingkittykait@gmail.com>
2023-03-11 21:33:26 -0800

    made index for both the routere and api

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./user-route');
+const thoughtRoutes = require('./thought-route');
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
